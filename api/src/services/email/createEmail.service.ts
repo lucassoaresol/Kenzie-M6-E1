c2bf95f2ec8ff3792bdbef6ff66bc16f76787282
Lucas <lucassoares2007@hotmail.com>
2023-03-24 01:47:08 -0300

    refactor: create email

--- api/src/services/email/createEmail.service.ts
+++ api/src/services/email/createEmail.service.ts
@@ -1,3 +1,4 @@
+import { ListEmail } from '@prisma/client';
 import { IEmailRequest } from '../../interfaces/email.interfaces';
 import prisma from '../../prisma';
 import { emailResponserSerializer } from '../../serializers/email.serializes';
@@ -5,16 +6,24 @@ import { emailResponserSerializer } from '../../serializers/email.serializes';
 const createEmailService = async (
   emailData: IEmailRequest,
   userId?: string,
+  contactId?: string,
 ) => {
+  let email: ListEmail;
   if (userId) {
-    const email = await prisma.listEmail.create({
-      data: { ...emailData, userId: userId },
-    });
-
-    return await emailResponserSerializer.validate(email, {
-      stripUnknown: true,
+    email = await prisma.listEmail.create({
+      data: { ...emailData, userId },
     });
   }
+
+  if (contactId) {
+    email = await prisma.listEmail.create({
+      data: { ...emailData, contactId },
+    });
+  }
+
+  return await emailResponserSerializer.validate(email, {
+    stripUnknown: true,
+  });
 };
 
 export default createEmailService;
